19833e56fb0f30ab27914e9e50f324ddcf5209bc
BenjaminWils <benjamin.59190@gmail.com>
2016-06-02 17:03:00 +0200

    Ajout suite des interfaces - Page accueil vendeur en cours

---
+++ app/src/main/java/com/ig2i/thegreenmagpie/buyer/BuyerHomepageActivity.java
@@ -0,0 +1,16 @@
+package com.ig2i.thegreenmagpie.buyer;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ig2i.thegreenmagpie.R;
+
+public class BuyerHomepageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buyer_homepage);
+    }
+}
