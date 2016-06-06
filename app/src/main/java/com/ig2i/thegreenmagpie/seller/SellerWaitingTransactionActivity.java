b98aa024a4a07a05149dd1ba01909b520147394f
BenjaminWils <bga.wils@gmail.com>
2016-06-06 10:00:32 +0200

    Création d'interface jusqu'à History et refonte balance management

---
+++ app/src/main/java/com/ig2i/thegreenmagpie/seller/SellerWaitingTransactionActivity.java
@@ -0,0 +1,16 @@
+package com.ig2i.thegreenmagpie.seller;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ig2i.thegreenmagpie.R;
+
+public class SellerWaitingTransactionActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_waiting_transaction);
+    }
+}
